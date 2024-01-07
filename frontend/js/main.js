c0e0a6bbfa378b41a64171d9a874c01ccd27b20c
PabloMas060 <pablo.mas.dev@gmail.com>
2024-01-07 06:39:30 -0300

    tp_fetch

---
+++ frontend/js/main.js
@@ -0,0 +1,60 @@
+window.onload = async () => {
+  const app = document.getElementById("root");
+  const container = document.createElement("div");
+  container.setAttribute("class", "container");
+  app.appendChild(container);
+
+  // Aqui debemos agregar nuestro fetch
+
+  try {
+
+    const response = await fetch('http://localhost:3031/api/movies');
+    const result = await response.json();
+
+    let data = result.data;
+
+    data.forEach((movie) => {
+      const card = document.createElement("div");
+      card.setAttribute("class", "card");
+
+      const h1 = document.createElement("h1");
+      h1.textContent = movie.title;
+      const h2 = document.createElement("h2");
+
+      const p = document.createElement("p");
+      p.textContent = `Rating: ${movie.rating}`;
+
+      const duracion = document.createElement("p");
+      duracion.textContent = `Duración: ${movie.length}`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+      if (movie.genre !== null) {
+        const genero = document.createElement("p");
+        genero.textContent = `Genero: ${movie.genre.name ? movie.genre.name : 'Sin especificar'}`;
+        card.appendChild(genero);
+      }
+      card.appendChild(duracion);
+
+      const link = document.createElement("a");
+      link.setAttribute("class", "btn")
+      link.setAttribute("href", `formulario.html?movie=${movie.id}&edit=true`)
+      link.textContent = "Editar"
+      card.appendChild(link);
+
+      const star = document.createElement("button");
+      star.classList.add("fa-regular", "fa-star"); 
+      star.textContent = "Agregar a favoritas"
+      star.setAttribute("title", "Agregar a favoritos"); 
+      card.appendChild(star);
+    });
+
+
+    console.log(result);
+
+  } catch (error) {
+    console.log(error)
+  }
+
+};
